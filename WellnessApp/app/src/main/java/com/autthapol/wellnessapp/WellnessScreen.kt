3a8d775aa264a66a41564c68e63396fc288e7b70
Autthapol Korkaew <polcpekusrc@gmail.com>
2024-05-08 16:18:22 +0700

    complete wellness app
    learn about state in jetpack compose

---
+++ WellnessApp/app/src/main/java/com/autthapol/wellnessapp/WellnessScreen.kt
@@ -0,0 +1,22 @@
+package com.autthapol.wellnessapp
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    viewModel: WellnessViewModel = viewModel(),
+) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        WellnessTaskList(list = viewModel.tasks, onCloseTask = { task ->
+            viewModel.remove(task)
+        }, onCheckedTask = { task, checked ->
+            viewModel.changeTaskChecked(task, checked)
+        })
+    }
+}
