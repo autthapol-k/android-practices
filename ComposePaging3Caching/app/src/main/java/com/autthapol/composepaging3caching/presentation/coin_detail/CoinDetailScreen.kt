e48741e6b5aa03f7b5ae295480c92537f3b19e30
Autthapol Korkaew <polcpekusrc@gmail.com>
2024-05-24 23:01:36 +0700

    add loading state on coin detail screen

--- ComposePaging3Caching/app/src/main/java/com/autthapol/composepaging3caching/presentation/coin_detail/CoinDetailScreen.kt
+++ ComposePaging3Caching/app/src/main/java/com/autthapol/composepaging3caching/presentation/coin_detail/CoinDetailScreen.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,6 +46,9 @@ fun CoinDetailScreen(
                     }
                 }
             }
+        }
+        if (state.isLoading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         }
     }
 }
